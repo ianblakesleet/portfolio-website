ca8de65b28683cdcbbe43b9fb5376f881bd2a555
ianblakesleet <ianblakesleet@gmail.com>
2022-09-01 20:52:26 -0400

    active links

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import styles from './about.module.css'
 const About = () => {
-	return <div>About</div>
+	return (
+		<div className={styles.aboutMe}>
+			<p>
+				Hello! My name is Ian Blakeslee, I am a full-stack web developer
+				who loves solving puzzles and learning new things! I graduated
+				from the Web Development program at Devmountain in July of 2022.
+				When I'm not coding, you can find me out in nature paddle
+				boarding, or snorkling.
+			</p>
+		</div>
+	)
 }
 
 export default About
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,8 @@ import styles from './home.module.css'
 const Home = () => {
 	return (
 		<div>
-			<h1>Hey, I'm Ian & I build things on the internet</h1>
+			<h1>Hey, I'm Ian &</h1>
+			<h1> I build things on the internet</h1>
 		</div>
 	)
 }
