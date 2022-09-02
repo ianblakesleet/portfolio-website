e197c93325757248e79072e1b7c98385a550a6c2
ianblakesleet <ianblakesleet@gmail.com>
2022-09-02 16:13:04 -0400

    about section completed, starting portfolio

--- src/components/About.js
+++ src/components/About.js
@@ -7,8 +7,8 @@ const About = () => {
 				Hello! My name is Ian Blakeslee, I am a full-stack web developer
 				who loves solving puzzles and learning new things! I graduated
 				from the Web Development program at Devmountain in July of 2022.
-				When I'm not coding, you can find me out in nature paddle
-				boarding, or snorkling.
+				When I'm not coding, you can find me in the gym, out in nature
+				paddle boarding, or snorkling
 			</p>
 		</div>
 	)
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import styles from './portfolio.module.css'
 
 const Portfolio = () => {
-	return <div>Portfolio</div>
+	return (
+		<div className={styles.transparentBox}>
+			<div className={styles.card}>
+				<h2>Task Manager</h2>
+				<p>IMAGE GOES HERE, CLICK FOR MODAL POPUP</p>
+			</div>
+			<div className={styles.card}>
+				<h2>LiveChat</h2>
+				<p>IMAGE GOES HERE, CLICK FOR MODAL POPUP</p>
+			</div>
+			<div className={styles.card}>
+				<h2>SAMPLE 3</h2>
+				<p>IMAGE GOES HERE, CLICK FOR MODAL POPUP</p>
+			</div>
+			<div className={styles.card}>
+				<h2>SAMPLE 4</h2>
+				<p>IMAGE GOES HERE, CLICK FOR MODAL POPUP</p>
+			</div>
+		</div>
+	)
 }
 
 export default Portfolio
